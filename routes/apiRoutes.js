6d916f8edb9843c7d7206240d18a4be0d9da8f47
Torontow <mtorontow0@gmail.com>
2020-12-15 16:27:14 -0500

    Delete Function Added
    Built route to delete notes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,23 +4,42 @@ module.exports = app => {
 
     // GET request reads db.json file and sends it as a response
 
-    // app.get('/api/notes', function(req, res) {
+    // app.get('/api/notes', (req, res) => {
     //     let notesData = JSON.parse(fs.readFileSync('./db/db.json','utf8'));
     //     res.json(notesData)
     // });
 
     // POST request receives a new note adds it to the db.json file and returns it to the client.
 
-    app.post('/api/notes', function(req, res) {
-        let notesData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    app.post('/api/notes',(req, res) => {
+        let notesData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
         let newNote = req.body;
         let uniqueID = (notesData.length).toString();
         newNote.id = uniqueID;
         notesData.push(newNote);
     
-        fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
-        console.log("Note saved to db.json. Content: ", newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+        console.log('Note saved to db.json. Content: ', newNote);
         res.json(notesData);
     })        
-
+    
+    // DELETE request deletes a note and returns the updated db.JSON file with newly assigned ids.
+    app.delete('/api/notes/:id', function(req, res) {
+        let notesData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        let noteID = req.params.id;
+        let newID = 0;
+        console.log(`Deleting note with ID ${noteID}`);
+        notesData = notesData.filter(currNote => {
+            return currNote.id != noteID;
+        })
+        
+        for (currNote of notesData) {
+            currNote.id = newID.toString();
+            newID++;
+        }
+    
+        fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
+        res.json(notesData);
+    })
+    
 }
